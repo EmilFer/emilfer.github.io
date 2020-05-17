85600eebb888f223e5a5ad54c5ef49ee2989faf0
Emil F <emil.fernlof@gmail.com>
2020-05-17 18:26:21 +0200

    Replace backdrop with Canvas

--- index.js
+++ index.js
@@ -2,21 +2,23 @@ window.onload = function() {
     generateBackdrop();
 };
 
+window.onresize = function() {
+    generateBackdrop();
+}
+
 function generateBackdrop() {
     var element = document.getElementById("backdrop");
-    var width = window.innerWidth;
-    var rootWidth = Math.sqrt(width);
+    var canvas = document.getElementById("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = element.offsetHeight;
 
-    for (var i = 0; i < rootWidth; i++) {
-        var newElement = document.createElement("div");
-        var size = randomInRange(10, rootWidth);
-        newElement.style.width = size;
-        newElement.style.height = size;
-        newElement.style.backgroundColor = "rgb(0, " + randomInRange(0, 255) + ", 0)";
-        newElement.style.margin = "5px";
-        newElement.style.display = "grid";
-        element.appendChild(newElement);
-    }
+    var ctx = canvas.getContext("2d");
+
+    ctx.moveTo(0,0);
+    ctx.lineTo(canvas.width, canvas.height);
+    ctx.stroke();
+
+    console.log(canvas.width + ", " + canvas.height)
 }
 
 function randomInRange(min, max) {
