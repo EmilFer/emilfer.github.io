4c23d298493946477d2d0dbb426f5757773926e1
Emil F <emil.fernlof@gmail.com>
2020-05-17 19:49:34 +0200

    We have green boxes

--- index.js
+++ index.js
@@ -11,40 +11,32 @@ function generateBackdrop() {
     var element = document.getElementById("backdrop");
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
-    var boxes;
+    var boxes = new Array(0);
 
     canvas.width = window.innerWidth;
     canvas.height = element.offsetHeight;
 
-    var minBoxSize = Math.sqrt(canvas.width);
-    var squares = minBoxSize * 5;
-    var verticalBoxes = Math.ceil(canvas.height / minBoxSize);
-    var horizontalBoxes = Math.ceil(canvas.width / minBoxSize);
+    var minBoxSize = Math.floor(Math.sqrt(canvas.width));
+    var boxAmount = Math.floor(minBoxSize * 5);
+    var horizontals = Math.ceil(canvas.width / minBoxSize);
+    var verticals = Math.ceil(canvas.height / minBoxSize);
 
-    boxes = new Array(horizontalBoxes);
-
-    for (var i = 0; i < boxes.length; i++) {
-        boxes[i] = new Array(verticalBoxes);
+    while (boxes.length < boxAmount) {
+        var x = randomIntInRange(0, horizontals) * minBoxSize;
+        var y = randomIntInRange(0, verticals) * minBoxSize;
+        var size = selectSize() * minBoxSize;
+        var color = randomLeafColor();
+        boxes.push(new Box(x, y, size, color));
     }
-
-    for (var i = 0; i < boxes.length; i++) {
-        for (var j = 0; j < boxes[i].length; j++) {
-            boxes[i][j] = new Box(minBoxSize, minBoxSize);
-        }
-    }
-
-    console.log(boxes.length + ", " + boxes[0].length);
 
     drawBackdrop(boxes, ctx);
 }
 
 function drawBackdrop(boxes, ctx) {
     for (var i = 0; i < boxes.length; i++) {
-        for (var j = 0; j < boxes[i].length; j++) {
-            var place = boxes[i][j].x;
-            var size = boxes[i][j].x - 1;
-            ctx.fillRect(i * place + 1, j * place + 1, size, size);
-        }
+        var box = boxes[i];
+        ctx.fillStyle = box.color;
+        ctx.fillRect(box.x, box.y, box.size, box.size);
     }
 }
 
@@ -53,17 +45,34 @@ function randomInRange(min, max) {
     return min + (max - min) * r;
 }
 
-class Square {
-    constructor() {
-        this.boxes = new Array(0);
-        this.color = "#00FF00"
-    }
+function randomIntInRange(min, max) {
+    var r = randomInRange(min - 1, max)
+    return Math.floor(r) + 1;
 }
 
+function selectSize() {
+    var r = Math.random();
+    if (r < 0.1) {
+        return 3;
+    }
+    if (r < 0.4) {
+        return 2;
+    }
+    return 1;
+}
+
+function randomLeafColor() {
+    var r = (50).toString(16).padEnd(2, "0");
+    var g = randomIntInRange(150, 255).toString(16).padEnd(2, "0");
+    var b = (100).toString(16).padEnd(2, "0");
+    return "#" + r + g + b;
+}
 
 class Box {
-    constructor(x, y) {
+    constructor(x, y, size, color) {
         this.x = x;
         this.y = y;
+        this.size = size;
+        this.color = color;
     }
 }
